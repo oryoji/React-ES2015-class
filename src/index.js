64e4fbbb188c9980bb7bcbd834742094f709368d
oryoji <60314819+oryoji@users.noreply.github.com>
2020-11-29 12:03:03 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ class Human {
     this.name = name;
     this.age = age;
   }
+
+  callMyProfile() {
+    console.log(this.name, this.age);
+  }
 }
 
 const Naoto = new Human("Naoto", 22);
 console.log(Naoto);
+Naoto.callMyProfile();
 
 const Nakata = new Human("Nakata", 36);
 console.log(Nakata);
+Nakata.callMyProfile();
