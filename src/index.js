436a6245bce9fab5231c4d422de776b012bd7315
oryoji <60314819+oryoji@users.noreply.github.com>
2020-11-29 11:37:51 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+class Human {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const Naoto = new Human("Naoto", 22);
+console.log(Naoto);
+
+const Nakata = new Human("Nakata", 36);
+console.log(Nakata);
